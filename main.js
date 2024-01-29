79579f083fe3b413bfeaf709cdc3d680801c325f
quentin-bmh <montoulieu.quentin@gmail.com>
2024-01-29 23:31:16 +0100

    transfert

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-document.addEventListener('scroll', function () {
-    // Calculer le pourcentage de défilement par rapport à la hauteur totale de la page
-    const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+document.addEventListener('DOMContentLoaded', function () {
+    var scrollbar = document.querySelector('.scrollbar');
+    var body = document.body;
+    var html = document.documentElement;
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
 
-    // Mettre à jour la largeur de la barre de défilement en fonction du pourcentage de défilement
-    document.querySelector('.scrollbar').style.width = scrollPercentage + '%';
+    window.addEventListener('scroll', function () {
+        var scrollTop = window.scrollY;
+        var documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        var windowHeight = window.innerHeight;
+
+        // Calculate the scroll progress as a percentage
+        var scrollProgress = ((scrollTop - navbarHeight) / (documentHeight - windowHeight - navbarHeight)) * 100;
+
+        // Ensure scrollProgress is within the 0 to 100 range
+        scrollProgress = Math.max(0, Math.min(scrollProgress, 100));
+
+        // Set the width of the scrollbar based on the scroll progress
+        scrollbar.style.width = scrollProgress + '%';
+    });
 });
